47c74efaf42aa613a0bfd6d9160699abbf4f2644
csanunkor <csanunkor@gmail.com>
2021-10-10 15:50:12 -0700

    Initial commit

---
+++ app/src/main/java/com/example/candystore/InsertActivity.java
@@ -0,0 +1,53 @@
+package com.example.candystore;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class InsertActivity extends AppCompatActivity {
+    private DatabaseManager dbManager;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        //YOUR CODE
+        dbManager = new DatabaseManager(this);
+        setContentView(R.layout.activity_insert);
+    }
+
+    public void insert(View v) {
+        // Retrieve name and price //YOUR CODE
+        EditText nameEditText = (EditText) findViewById(R.id.input_name);
+        EditText priceEditText = (EditText) findViewById(R.id.input_price);
+        String name = nameEditText.getText().toString();
+        String priceString = priceEditText.getText().toString();
+
+        // insert new candy in database
+        try{
+            double price = Double.parseDouble(priceString);
+            Candy candy = new Candy(0,name,price);
+            dbManager.insert(candy);
+            Toast.makeText(this, "Candy added", Toast.LENGTH_SHORT).show();
+        } catch(NumberFormatException nfe){
+            Toast.makeText(this, "Price error", Toast.LENGTH_LONG).show();
+        }
+
+        ArrayList<Candy> candies = dbManager.selectAll();
+        for( Candy candy : candies ) {
+            Log.w("MainActivity", "candy = " + candy.toString());
+        }
+
+        // clear data
+        nameEditText.setText("");
+        priceEditText.setText(""); }
+
+    public void goBack(View v) {
+        InsertActivity.this.finish();
+    }
+}
---
+++ app/src/main/java/com/example/candystore/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.candystore;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //this sets this toolbar to act as the action bar.
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_add:
+                Log.w("MainActivity","Add selected");
+                Intent insertIntent = new Intent(this, InsertActivity.class);
+                this.startActivity(insertIntent);
+                return true;
+            case R.id.action_delete:
+                Log.w("MainActivity","Delete selected");
+                Intent deleteIntent = new Intent(this, DeleteActivity.class);
+                this.startActivity(deleteIntent);
+                return true;
+            case R.id.action_update:
+                Log.w("MainActivity","Update selected");
+                Intent updateIntent = new Intent(this, UpdateActivity.class);
+                this.startActivity(updateIntent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
